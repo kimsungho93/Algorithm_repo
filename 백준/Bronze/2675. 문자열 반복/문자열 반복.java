92d47777512aced6a0af56d852ca718e5d005b24
kimsungho93 <87847853+kimsungho93@users.noreply.github.com>
2024-03-27 15:55:25 +0900

    [Bronze II] Title: 문자열 반복, Time: 120 ms, Memory: 13972 KB -BaekjoonHub

--- 백준/Bronze/2675. 문자열 반복/문자열 반복.java
+++ 백준/Bronze/2675. 문자열 반복/문자열 반복.java
@@ -1,18 +1,25 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int T = sc.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int T = Integer.parseInt(br.readLine());
+
         for (int i = 0; i < T; i++) {
-            int R = sc.nextInt();
-            String S = sc.next();
-            for (int j = 0; j < S.length(); j++) {
-                for (int k = 0; k < R; k++) {
-                    System.out.print(S.charAt(j));
+            String[] input = br.readLine().split(" ");
+            int R = Integer.parseInt(input[0]);
+            String S = input[1];
+
+            StringBuilder P = new StringBuilder();
+            for (char c : S.toCharArray()) {
+                for (int j = 0; j < R; j++) {
+                    P.append(c);
                 }
             }
-            System.out.println();
+            System.out.println(P);
         }
     }
 }
