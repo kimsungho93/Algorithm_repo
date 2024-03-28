ba3f0fc7fcb5c0fba2ab3213b240959e49308cd4
kimsungho93 <87847853+kimsungho93@users.noreply.github.com>
2024-03-28 14:34:40 +0900

    [Silver III] Title: 파일 정리, Time: 836 ms, Memory: 49160 KB -BaekjoonHub

--- 백준/Silver/20291. 파일 정리/파일 정리.java
+++ 백준/Silver/20291. 파일 정리/파일 정리.java
@@ -3,8 +3,8 @@ import java.io.BufferedWriter;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.io.IOException;
-import java.util.HashMap;
 import java.util.StringTokenizer;
+import java.util.TreeMap;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -12,29 +12,20 @@ public class Main {
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         int N = Integer.parseInt(br.readLine());
-        HashMap<String, Integer> map = new HashMap<>();
+        TreeMap<String, Integer> map = new TreeMap<>();
 
         for (int i = 0; i < N; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine(), ".");
-            String fileName = st.nextToken();
-            String extension = st.nextToken();
+            st.nextToken(); 
+            String extension = st.nextToken(); 
 
-            if (!map.containsKey(extension)) {
-                map.put(extension, map.getOrDefault(extension, 0) + 1);
-            } else {
-                map.put(extension, map.get(extension) + 1);
-            }
+            map.put(extension, map.getOrDefault(extension, 0) + 1);
         }
 
-        map.entrySet().stream()
-            .sorted((e1, e2) -> e1.getKey().compareTo(e2.getKey()))
-            .forEach(e -> {
-                try {
-                    bw.write(e.getKey() + " " + e.getValue() + "\n");
-                } catch (IOException e1) {
-                    e1.printStackTrace();
-                }
-            });
+        for (String key : map.keySet()) {
+            bw.write(key + " " + map.get(key) + "\n");
+        }
+
         bw.flush();
         br.close();
         bw.close();
