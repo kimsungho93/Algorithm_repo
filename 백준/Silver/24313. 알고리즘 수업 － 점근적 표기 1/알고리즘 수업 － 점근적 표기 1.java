e6405e2fd2525cfce35216393a37333d27e6fb27
kimsungho93 <87847853+kimsungho93@users.noreply.github.com>
2024-03-29 12:51:00 +0900

    [Silver V] Title: 알고리즘 수업 - 점근적 표기 1, Time: 120 ms, Memory: 14320 KB -BaekjoonHub

--- 백준/Silver/24313. 알고리즘 수업 － 점근적 표기 1/알고리즘 수업 － 점근적 표기 1.java
+++ 백준/Silver/24313. 알고리즘 수업 － 점근적 표기 1/알고리즘 수업 － 점근적 표기 1.java
@@ -1,36 +1,26 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.io.IOException;
 import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         StringTokenizer st = new StringTokenizer(br.readLine());
         int a1 = Integer.parseInt(st.nextToken());
-        int a0 = Integer.parseInt(st.nextToken());
-        int c = Integer.parseInt(br.readLine());
+        int a0 = Integer.parseInt(st.nextToken()); 
+        int c = Integer.parseInt(br.readLine()); 
         int n0 = Integer.parseInt(br.readLine());
 
-        if (checkOnotation(a1, a0, c, n0)) {
-            bw.write("1\n");
-        } else {
-            bw.write("0\n");
-        }
-        bw.flush();
-        bw.close();
-    }
-
-    private static boolean checkOnotation(int a1, int a0, int c, int n0) {
+        boolean isSatisfied = true;
         for (int n = n0; n <= n0 + 100; n++) {
             if (a1 * n + a0 > c * n) {
-                return false;
+                isSatisfied = false;
+                break;
             }
         }
-        return true;
+
+        System.out.println(isSatisfied ? 1 : 0);
     }
 }
