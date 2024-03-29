4e7c02589e7c14a17fa392a3c9fbb5f77d89f6b7
kimsungho93 <87847853+kimsungho93@users.noreply.github.com>
2024-03-29 11:31:54 +0900

    [Silver III] Title: N과 M (1), Time: 260 ms, Memory: 22900 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.java
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -1,12 +1,13 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.IOException;
 import java.util.StringTokenizer;
 
 public class Main {
     static int N, M;
     static int[] arr;
     static boolean[] visit;
+    static StringBuilder sb = new StringBuilder();
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -19,14 +20,15 @@ public class Main {
         visit = new boolean[N + 1];
 
         dfs(0);
+        System.out.print(sb);
     }
 
     public static void dfs(int depth) {
         if (depth == M) {
             for (int val : arr) {
-                System.out.print(val + " ");
+                sb.append(val).append(' ');
             }
-            System.out.println();
+            sb.append('\n');
             return;
         }
 
