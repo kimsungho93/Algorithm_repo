993f6383bfceaf2b8341e676234ed508bf6d9dcb
kimsungho93 <87847853+kimsungho93@users.noreply.github.com>
2024-03-30 11:54:53 +0900

    [Silver IV] Title: 듣보잡, Time: 280 ms, Memory: 25960 KB -BaekjoonHub

--- 백준/Silver/1764. 듣보잡/듣보잡.java
+++ 백준/Silver/1764. 듣보잡/듣보잡.java
@@ -3,7 +3,7 @@ import java.io.InputStreamReader;
 import java.io.IOException;
 import java.util.HashSet;
 import java.util.ArrayList;
-import java.util.Collections;
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -13,22 +13,24 @@ public class Main {
         int N = Integer.parseInt(input[0]);
         int M = Integer.parseInt(input[1]);
 
-        HashSet<String> notHeard = new HashSet<>();
+        HashSet<String> set = new HashSet<>();
         for (int i = 0; i < N; i++) {
-            notHeard.add(br.readLine());
+            set.add(br.readLine());
         }
 
-        ArrayList<String> result = new ArrayList<>();
+        ArrayList<String> tempList = new ArrayList<>();
         for (int i = 0; i < M; i++) {
             String name = br.readLine();
-            if (notHeard.contains(name)) {
-                result.add(name);
+            if (set.contains(name)) {
+                tempList.add(name);
             }
         }
 
-        Collections.sort(result);
+        String[] result = tempList.toArray(new String[0]);
 
-        System.out.println(result.size());
+        Arrays.sort(result);
+
+        System.out.println(result.length);
         StringBuilder sb = new StringBuilder();
         for (String name : result) {
             sb.append(name).append("\n");
