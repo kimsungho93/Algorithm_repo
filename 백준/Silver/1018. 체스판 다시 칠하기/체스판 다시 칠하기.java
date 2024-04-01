c9ce91729d3df7bc4ff0b8b6e58438467f6dbb01
kimsungho93 <87847853+kimsungho93@users.noreply.github.com>
2024-04-01 12:00:37 +0900

    [Silver IV] Title: 체스판 다시 칠하기, Time: 84 ms, Memory: 11708 KB -BaekjoonHub

---
+++ 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.java
@@ -0,0 +1,62 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        char[][] whiteFirstPattern = {
+                {'W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'},
+                {'B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'},
+                {'W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'},
+                {'B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'},
+                {'W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'},
+                {'B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'},
+                {'W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'},
+                {'B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'}
+        };
+
+        char[][] blackFirstPattern = {
+                {'B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'},
+                {'W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'},
+                {'B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'},
+                {'W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'},
+                {'B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'},
+                {'W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'},
+                {'B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'},
+                {'W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'}
+        };
+
+        String[] nm = br.readLine().split(" ");
+        int N = Integer.parseInt(nm[0]);
+        int M = Integer.parseInt(nm[1]);
+
+        char[][] board = new char[N][M];
+        for (int i = 0; i < N; i++) {
+            board[i] = br.readLine().toCharArray();
+        }
+
+        int min = Integer.MAX_VALUE;
+        for (int i = 0; i <= N - 8; i++) {
+            for (int j = 0; j <= M - 8; j++) {
+                min = Math.min(min, getMin(board, whiteFirstPattern, i, j));
+                min = Math.min(min, getMin(board, blackFirstPattern, i, j));
+            }
+        }
+
+        System.out.println(min);
+    }
+
+    public static int getMin(char[][] board, char[][] pattern, int x, int y) {
+        int count = 0;
+        for (int i = x; i < x + 8; i++) {
+            for (int j = y; j < y + 8; j++) {
+                if (board[i][j] != pattern[i - x][j - y]) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+}
