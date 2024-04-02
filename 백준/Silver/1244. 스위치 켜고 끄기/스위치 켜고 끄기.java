33c59ec569ca6099b57034de773e71c9d321f98d
kimsungho93 <87847853+kimsungho93@users.noreply.github.com>
2024-04-02 09:19:27 +0900

    [Silver IV] Title: 스위치 켜고 끄기, Time: 80 ms, Memory: 11540 KB -BaekjoonHub

--- 백준/Silver/1244. 스위치 켜고 끄기/스위치 켜고 끄기.java
+++ 백준/Silver/1244. 스위치 켜고 끄기/스위치 켜고 끄기.java
@@ -5,45 +5,49 @@ import java.io.InputStreamReader;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
-        int switchCount = Integer.parseInt(br.readLine()); // 스위치의 개수
-        String[] switchStates = br.readLine().split(" "); // 스위치의 초기 상태
-        int studentCount = Integer.parseInt(br.readLine()); // 학생의 수
-        
-        boolean[] switches = new boolean[switchCount + 1]; // 스위치 상태 배열 (1부터 시작)
-        
-        // 스위치 초기 상태 설정
+
+        int switchCount = Integer.parseInt(br.readLine());
+        String[] state = br.readLine().split(" ");
+        int studentCount = Integer.parseInt(br.readLine());
+        boolean[] arr = new boolean[switchCount + 1];
+
         for (int i = 1; i <= switchCount; i++) {
-            switches[i] = switchStates[i - 1].equals("1");
+            arr[i] = state[i - 1].equals("1");
         }
-        
-        // 학생별로 스위치 조작
+
         for (int i = 0; i < studentCount; i++) {
             String[] input = br.readLine().split(" ");
-            int gender = Integer.parseInt(input[0]); // 학생의 성별
-            int num = Integer.parseInt(input[1]); // 학생이 받은 번호
-            
-            if (gender == 1) { // 남학생의 경우
+            int gender = Integer.parseInt(input[0]);
+            int num = Integer.parseInt(input[1]);
+
+            if (gender == 1) {
                 for (int j = num; j <= switchCount; j += num) {
-                    switches[j] = !switches[j];
+                    arr[j] = !arr[j];
                 }
-            } else { // 여학생의 경우
-                switches[num] = !switches[num];
+            } else {
+                arr[num] = !arr[num];
                 int left = num - 1;
                 int right = num + 1;
-                while (left >= 1 && right <= switchCount && switches[left] == switches[right]) {
-                    switches[left] = !switches[left];
-                    switches[right] = !switches[right];
+                while (left >= 1 && right <= switchCount && arr[left] == arr[right]) {
+                    arr[left] = !arr[left];
+                    arr[right] = !arr[right];
                     left--;
                     right++;
                 }
             }
         }
-        
-        // 결과 출력
+
+        StringBuilder sb = new StringBuilder();
         for (int i = 1; i <= switchCount; i++) {
-            System.out.print(switches[i] ? "1 " : "0 ");
-            if (i % 20 == 0) System.out.println(); // 20개의 스위치마다 줄바꿈
+            if (arr[i]) {
+                sb.append("1 ");
+            } else {
+                sb.append("0 ");
+            }
+            if (i % 20 == 0) {
+                sb.append("\n");
+            } 
         }
+        System.out.println(sb);
     }
 }
