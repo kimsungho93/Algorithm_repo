224d5d5291be262e3072f46f809e6ef42630cc3d
kimsungho93 <87847853+kimsungho93@users.noreply.github.com>
2024-04-02 11:29:05 +0900

    [Gold V] Title: IPv6, Time: 244 ms, Memory: 18724 KB -BaekjoonHub

--- 백준/Gold/3107. IPv6/IPv6.java
+++ 백준/Gold/3107. IPv6/IPv6.java
@@ -1,45 +1,38 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class Main {
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String[] input = br.readLine().split(":");
-        int part = input.length;
-        for (String tmp : input) {
-            if(tmp.length() == 0) part--;
-        }
+        String ip = br.readLine();
         StringBuilder sb = new StringBuilder();
-        int idx = 0;
-        int cnt = 0;
-        while ( idx < input.length ) {
-            String tmp = input[idx];
-            if ( tmp.length() == 0 && part < 8){
-                sb.append("0000");
-                sb.append(":");
-                cnt++;
-                part++;
-            } else if ( tmp.length() == 0 ) {
-                idx++;
-                continue;
-            } else if ( tmp.length() != 0) {
-                for(int i = 0 ; i < 4 - tmp.length() ; i++){
-                    sb.append("0");
-                }
-                sb.append(tmp);
-                idx++;
-                cnt++;
-                if(cnt < 8) sb.append(":");
+
+        boolean hasEmpty = ip.contains("::");
+        if (hasEmpty) {
+            if (ip.equals("::")) {
+                System.out.println("0000:0000:0000:0000:0000:0000:0000:0000");
+                return;
             }
+            String[] parts = ip.split("::", -1);
+            String pre = parts[0];
+            String post = parts.length > 1 ? parts[1] : "";
+            int totalSegs = (pre.isEmpty() ? 0 : pre.split(":").length) + (post.isEmpty() ? 0 : post.split(":").length);
+            int segsToFill = 8 - totalSegs;
+
+            ip = pre + ":" + IntStream.range(0, segsToFill).mapToObj(i -> "0000").collect(Collectors.joining(":")) + ":" + post;
+            if (ip.startsWith(":")) ip = ip.substring(1);
+            if (ip.endsWith(":")) ip = ip.substring(0, ip.length() - 1);
         }
-        while (cnt < 8) {
-            sb.append("0000");
-            cnt++;
-            if(cnt < 8) sb.append(":");
+
+        String[] segs = ip.split(":", -1);
+        for (int i = 0; i < segs.length; i++) {
+            sb.append(String.format("%1$" + 4 + "s", segs[i]).replace(' ', '0'));
+            if (i < segs.length - 1) sb.append(":");
         }
+
         System.out.println(sb);
     }
 }
