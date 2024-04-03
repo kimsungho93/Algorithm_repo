e08a6f63fb2a307498c65541a196ccce5d8d8f5c
kimsungho93 <87847853+kimsungho93@users.noreply.github.com>
2024-04-03 16:44:34 +0900

    [Gold V] Title: AC, Time: 748 ms, Memory: 103988 KB -BaekjoonHub

--- 백준/Gold/5430. AC/AC.java
+++ 백준/Gold/5430. AC/AC.java
@@ -7,36 +7,36 @@ import java.util.Deque;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int T = Integer.parseInt(br.readLine()); // 테스트 케이스 개수
+        int T = Integer.parseInt(br.readLine()); 
 
         while (T-- > 0) {
-            String functions = br.readLine(); // 수행할 함수
-            int n = Integer.parseInt(br.readLine()); // 배열의 크기
-            String nums = br.readLine(); // 배열 원소
+            String functions = br.readLine(); 
+            int n = Integer.parseInt(br.readLine()); 
+            String nums = br.readLine(); 
             nums = nums.substring(1, nums.length() - 1);
             String[] splitNums = nums.split(",");
 
             Deque<Integer> deque = new ArrayDeque<>();
             for (int i = 0; i < n; i++) {
-                deque.addLast(Integer.parseInt(splitNums[i])); // Deque에 원소 추가
+                deque.addLast(Integer.parseInt(splitNums[i]));
             }
 
-            boolean isReversed = false; // 뒤집힘 상태 표시
-            boolean hasError = false; // 에러 발생 여부
+            boolean isReversed = false;
+            boolean hasError = false;
 
             for (char function : functions.toCharArray()) {
                 if (function == 'R') {
-                    isReversed = !isReversed; // 뒤집힘 상태 토글
+                    isReversed = !isReversed;
                 } else if (function == 'D') {
                     if (deque.isEmpty()) {
                         System.out.println("error");
                         hasError = true;
                         break;
                     } else {
-                        if (isReversed) {
-                            deque.removeLast(); // 뒤에서 원소 제거
+                        if ((isReversed)) {
+                            deque.removeLast();
                         } else {
-                            deque.removeFirst(); // 앞에서 원소 제거
+                            deque.removeFirst();
                         }
                     }
                 }
