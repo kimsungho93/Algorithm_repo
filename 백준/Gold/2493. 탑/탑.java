21b1cd5fe3768f2b1479227d526d27fd87840c00
kimsungho93 <87847853+kimsungho93@users.noreply.github.com>
2024-04-03 13:06:54 +0900

    [Gold V] Title: 탑, Time: 544 ms, Memory: 115264 KB -BaekjoonHub

--- 백준/Gold/2493. 탑/탑.java
+++ 백준/Gold/2493. 탑/탑.java
@@ -1,38 +1,30 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
-import java.util.Stack;
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine()); // 타워의 수
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        String[] heights = br.readLine().split(" ");
-        Stack<Tower> stack = new Stack<>();
-        int[] answer = new int[N]; // 각 타워에서 수신하는 타워의 번호를 저장할 배열
+        Deque<Tower> stack = new ArrayDeque<>();
+        int[] answer = new int[N];
 
         for (int i = 0; i < N; i++) {
-            int height = Integer.parseInt(heights[i]); // 현재 타워의 높이
+            int height = Integer.parseInt(st.nextToken());
 
-            while (!stack.isEmpty()) {
-                // 스택이 비어있지 않고, 현재 타워의 높이가 스택의 top에 있는 타워의 높이보다 크면
-                // 스택에서 해당 타워를 제거
-                if (stack.peek().height < height) {
-                    stack.pop();
-                } else {
-                    // 현재 타워의 높이가 스택의 top에 있는 타워의 높이보다 작거나 같으면
-                    // 스택의 top에 있는 타워가 현재 타워의 신호를 수신
-                    answer[i] = stack.peek().index + 1;
-                    break;
-                }
+            while (!stack.isEmpty() && stack.peek().height < height) {
+                stack.pop();
             }
 
-            // 현재 타워를 스택에 추가
+            answer[i] = stack.isEmpty() ? 0 : stack.peek().index + 1;
             stack.push(new Tower(i, height));
         }
 
-        // 결과 출력
         StringBuilder sb = new StringBuilder();
         for (int num : answer) {
             sb.append(num).append(" ");
@@ -50,3 +42,4 @@ class Tower {
         this.height = height;
     }
 }
+
