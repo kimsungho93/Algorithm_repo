c958360a799b134706076175a27871c549fdee7c
kimsungho93 <87847853+kimsungho93@users.noreply.github.com>
2024-04-04 12:07:09 +0900

    [Gold IV] Title: 세 수의 합, Time: 376 ms, Memory: 41888 KB -BaekjoonHub

--- 백준/Gold/2295. 세 수의 합/세 수의 합.java
+++ 백준/Gold/2295. 세 수의 합/세 수의 합.java
@@ -2,6 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
+import java.util.TreeSet;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -15,26 +16,25 @@ public class Main {
 
         Arrays.sort(num);
 
-        int[] sum = new int[N * (N + 1) / 2];
-        int count = 0;
+        TreeSet<Integer> sumSet = new TreeSet<>();
         for (int i = 0; i < N; i++) {
             for (int j = i; j < N; j++) {
-                sum[count++] = num[i] + num[j];
+                sumSet.add(num[i] + num[j]);
             }
         }
-
-        Arrays.sort(sum, 0, count - 1);
 
         int answer = 0;
         for (int i = N - 1; i >= 0; i--) {
-            for (int j = i; j >= 0; j--) {
+            for (int j = i - 1; j >= 0; j--) {
                 int target = num[i] - num[j];
-                if (Arrays.binarySearch(sum, 0, count - 1, target) >= 0) {
-                    answer = Math.max(answer, num[i]);
-                    break;
+                if (sumSet.contains(target)) {
+                    answer = num[i];
+                    break; 
                 }
             }
+            if (answer > 0) break; 
         }
+
 
         System.out.println(answer);
     }
