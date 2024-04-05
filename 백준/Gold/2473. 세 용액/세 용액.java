4402b1659522d5ae7ca68287865e0ec290d82f4a
kimsungho93 <87847853+kimsungho93@users.noreply.github.com>
2024-04-05 11:29:05 +0900

    [Gold III] Title: 세 용액, Time: 164 ms, Memory: 14128 KB -BaekjoonHub

--- 백준/Gold/2473. 세 용액/세 용액.java
+++ 백준/Gold/2473. 세 용액/세 용액.java
@@ -2,22 +2,21 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-        long[] solutions = new long[N];
+        long[] arr = new long[N];
 
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        String[] input = br.readLine().split(" ");
         for (int i = 0; i < N; i++) {
-            solutions[i] = Long.parseLong(st.nextToken());
+            arr[i] = Long.parseLong(input[i]);
         }
 
-        Arrays.sort(solutions);
+        Arrays.sort(arr);
 
-        long minValue = Long.MAX_VALUE;
+        long min = Long.MAX_VALUE;
         long[] result = new long[3];
 
         for (int i = 0; i < N - 2; i++) {
@@ -25,14 +24,14 @@ public class Main {
             int right = N - 1;
 
             while (left < right) {
-                long sum = solutions[i] + solutions[left] + solutions[right];
+                long sum = arr[i] + arr[left] + arr[right];
                 long absSum = Math.abs(sum);
 
-                if (absSum < minValue) {
-                    minValue = absSum;
-                    result[0] = solutions[i];
-                    result[1] = solutions[left];
-                    result[2] = solutions[right];
+                if (absSum < min) {
+                    min = absSum;
+                    result[0] = arr[i];
+                    result[1] = arr[left];
+                    result[2] = arr[right];
                 }
 
                 if (sum > 0) {
