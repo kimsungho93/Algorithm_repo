c0a63730ca3042f6cfd07d8fa557636a288f1797
kimsungho93 <87847853+kimsungho93@users.noreply.github.com>
2024-04-05 09:48:44 +0900

    [Silver II] Title: 나무 자르기, Time: 1060 ms, Memory: 139208 KB -BaekjoonHub

---
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.java
@@ -0,0 +1,46 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] input = br.readLine().split(" ");
+        int N = Integer.parseInt(input[0]);
+        int M = Integer.parseInt(input[1]);
+
+        int[] trees = new int[N];
+
+        String[] height = br.readLine().split(" ");
+        IntStream.range(0, N).forEach(i -> trees[i] = Integer.parseInt(height[i]));
+        Arrays.sort(trees);
+
+        int left = 0;
+        int right = trees[N - 1];
+        int result = 0;
+
+        while (left <= right) {
+            int mid = (left + right) / 2;
+
+            long sum = 0;
+
+            for (int tree : trees) {
+                if (tree > mid) {
+                    sum += tree - mid;
+                }
+            }
+
+            if (sum >= M) {
+                result = mid;
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        System.out.println(result);
+    }
+}
