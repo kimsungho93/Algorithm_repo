c61b05b6913023c2c7bedd76e61ff0a33aa16b9e
kimsungho93 <87847853+kimsungho93@users.noreply.github.com>
2024-04-05 11:09:14 +0900

    [Gold V] Title: 두 용액, Time: 288 ms, Memory: 32712 KB -BaekjoonHub

--- 백준/Gold/2470. 두 용액/두 용액.java
+++ 백준/Gold/2470. 두 용액/두 용액.java
@@ -1,42 +1,41 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-        long[] solutions = new long[N];
-        
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        long[] arr = new long[N];
+
+        String[] input = br.readLine().split(" ");
         for (int i = 0; i < N; i++) {
-            solutions[i] = Long.parseLong(st.nextToken());
+            arr[i] = Long.parseLong(input[i]);
         }
+
+        Arrays.sort(arr);
         
-        Arrays.sort(solutions);
-        
-        int left = 0, right = N - 1;
-        long answerLeft = solutions[left], answerRight = solutions[right];
-        long minDiff = Math.abs(answerLeft + answerRight);
+        int left = 0;
+        int right = N - 1;
+        long cursorL = arr[left], cursorR = arr[right];
+        long min = Math.abs(cursorL + cursorR);
 
         while (left < right) {
-            long sum = solutions[left] + solutions[right];
+            long sum = arr[left] + arr[right];
             long diff = Math.abs(sum);
-            
-            if (diff < minDiff) {
-                minDiff = diff;
-                answerLeft = solutions[left];
-                answerRight = solutions[right];
+
+            if (diff < min) {
+                min = diff;
+                cursorL = arr[left];
+                cursorR = arr[right];
             }
-            
+
             if (sum > 0) {
                 right--;
             } else {
                 left++;
             }
         }
-        
-        System.out.println(answerLeft + " " + answerRight);
+        System.out.println(cursorL + " " + cursorR);
     }
 }
