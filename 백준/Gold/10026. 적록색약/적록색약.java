18d639ffd1464817c6b20ad8152103b5fde6f235
kimsungho93 <87847853+kimsungho93@users.noreply.github.com>
2024-04-06 23:00:39 +0900

    [Gold V] Title: 적록색약, Time: 156 ms, Memory: 16316 KB -BaekjoonHub

--- 백준/Gold/10026. 적록색약/적록색약.java
+++ 백준/Gold/10026. 적록색약/적록색약.java
@@ -1,54 +1,53 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
     static char[][] grid;
     static boolean[][] visited;
     static int N;
-    static int[] dx = {-1, 1, 0, 0}; // 상, 하, 좌, 우
-    static int[] dy = {0, 0, -1, 1}; // 상, 하, 좌, 우
-    
-    public static void main(String[] args) throws Exception {
+    static int[] dx = {-1, 1, 0, 0};
+    static int[] dy = {0, 0, -1, 1};
+
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
         grid = new char[N][N];
-        
+
         for (int i = 0; i < N; i++) {
             grid[i] = br.readLine().toCharArray();
         }
         
-        System.out.println(countZones(false) + " " + countZones(true));
+        System.out.println(countArea(false) + " " + countArea(true));
     }
     
-    static int countZones(boolean colorBlindness) {
+    private static int countArea(boolean isColorBlind) {
         visited = new boolean[N][N];
         int count = 0;
-        
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < N; j++) {
                 if (!visited[i][j]) {
-                    dfs(i, j, grid[i][j], colorBlindness);
+                    dfs(i, j, grid[i][j], isColorBlind);
                     count++;
                 }
             }
         }
-        
         return count;
     }
-    
-    static void dfs(int x, int y, char color, boolean colorBlindness) {
+
+    private static void dfs(int x, int y, char color, boolean isColorBlind) {
         visited[x][y] = true;
         for (int i = 0; i < 4; i++) {
-            int nx = x + dx[i];
-            int ny = y + dy[i];
-            
-            if (nx >= 0 && nx < N && ny >= 0 && ny < N && !visited[nx][ny]) {
-                if (colorBlindness && (color == 'R' || color == 'G')) {
-                    if (grid[nx][ny] == 'R' || grid[nx][ny] == 'G') {
-                        dfs(nx, ny, color, colorBlindness);
+            int nextX = x + dx[i];
+            int nextY = y + dy[i];
+
+            if (nextX >= 0 && nextX < N && nextY >= 0 && nextY < N && !visited[nextX][nextY]) {
+                if (isColorBlind && (color == 'R' || color == 'G')) {
+                    if (grid[nextX][nextY] == 'R' || grid[nextX][nextY] == 'G') {
+                        dfs(nextX, nextY, color, isColorBlind);
                     }
-                } else if (grid[nx][ny] == color) {
-                    dfs(nx, ny, color, colorBlindness);
+                } else if (grid[nextX][nextY] == color) {
+                    dfs(nextX, nextY, color, isColorBlind);
                 }
             }
         }
