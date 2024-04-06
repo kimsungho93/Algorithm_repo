096be02ee49d645f2e04e1d8714ce88932a85bb3
kimsungho93 <87847853+kimsungho93@users.noreply.github.com>
2024-04-06 13:57:51 +0900

    [Gold V] Title: 로봇 프로젝트, Time: 2340 ms, Memory: 312364 KB -BaekjoonHub

--- 백준/Gold/3649. 로봇 프로젝트/로봇 프로젝트.java
+++ 백준/Gold/3649. 로봇 프로젝트/로봇 프로젝트.java
@@ -7,47 +7,34 @@ import java.util.StringTokenizer;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String line;
-        
-        while ((line = br.readLine()) != null && !line.isEmpty()) {
-            int x = Integer.parseInt(line) * 10000000; // 구멍의 너비(cm -> nm 변환)
-            int n = Integer.parseInt(br.readLine()); // 레고 조각의 수
+        String input;
+
+        while ((input = br.readLine()) != null && !input.isEmpty()) {
+            int x = Integer.parseInt(input) * 10000000;
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int n = Integer.parseInt(st.nextToken());
             int[] pieces = new int[n];
-            
+
             for (int i = 0; i < n; i++) {
-                pieces[i] = Integer.parseInt(br.readLine());
+                st = new StringTokenizer(br.readLine());
+                pieces[i] = Integer.parseInt(st.nextToken());
             }
-            
-            Arrays.sort(pieces); // 레고 조각 정렬
-            
-            boolean found = false;
-            for (int i = 0; i < n; i++) {
+
+            Arrays.sort(pieces);
+
+            boolean isExist = false;
+            for (int i = 0; i < n && !isExist; i++) {
                 int target = x - pieces[i];
-                if (binarySearch(pieces, target, i + 1, n - 1)) { // 이분 탐색
+                int index = Arrays.binarySearch(pieces, target);
+                if (index >= 0 && index != i) {
                     System.out.println("yes " + pieces[i] + " " + target);
-                    found = true;
-                    break;
+                    isExist = true;
                 }
             }
-            
-            if (!found) {
+
+            if (!isExist) {
                 System.out.println("danger");
             }
         }
-    }
-    
-    // 이분 탐색 메소드
-    private static boolean binarySearch(int[] pieces, int target, int start, int end) {
-        while (start <= end) {
-            int mid = (start + end) / 2;
-            if (pieces[mid] == target) {
-                return true;
-            } else if (pieces[mid] < target) {
-                start = mid + 1;
-            } else {
-                end = mid - 1;
-            }
-        }
-        return false;
     }
 }
