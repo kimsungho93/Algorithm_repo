5c307e78b9943f311c20b35e32bef402662cbbf2
kimsungho93 <87847853+kimsungho93@users.noreply.github.com>
2024-04-09 00:00:55 +0900

    [Bronze I] Title: 팰린드롬수, Time: 116 ms, Memory: 14108 KB -BaekjoonHub

--- 백준/Bronze/1259. 팰린드롬수/팰린드롬수.java
+++ 백준/Bronze/1259. 팰린드롬수/팰린드롬수.java
@@ -1,32 +1,36 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
-
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
 
         while (true) {
-            String input = br.readLine();
-            if (input.equals("0")) {
-                break;
-            }
-            int left = 0, right = input.length() - 1;
-            while (left <= right) {
-                if (input.charAt(left) != input.charAt(right)) {
-                    sb.append("no\n");
-                    break;
-                }
-                left++;
-                right--;
-                
-                if (left > right) {
-                    sb.append("yes\n");
-                }
+            String line = br.readLine();
+            if (line.equals("0")) break; 
+
+            if (isPalindrome(line)) {
+                sb.append("yes\n");
+            } else {
+                sb.append("no\n");
             }
         }
         System.out.println(sb);
+    }
+
+    private static boolean isPalindrome(String num) {
+        int left = 0;
+        int right = num.length() - 1;
+
+        while (left < right) {
+            if (num.charAt(left) != num.charAt(right)) {
+                return false; 
+            }
+            left++;
+            right--;
+        }
+
+        return true; 
     }
 }
