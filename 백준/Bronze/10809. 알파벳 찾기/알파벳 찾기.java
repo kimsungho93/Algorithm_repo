ffecce057e0d451d45d9734e91fc306a7ed56433
kimsungho93 <87847853+kimsungho93@users.noreply.github.com>
2024-04-09 00:17:53 +0900

    [Bronze II] Title: 알파벳 찾기, Time: 124 ms, Memory: 14260 KB -BaekjoonHub

--- 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.java
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.java
@@ -1,21 +1,23 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 
 public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringBuilder sb = new StringBuilder();
+    static int[] alphabet = new int[26];
     public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String word = br.readLine();
-        int[] alphabet = new int[26];
-
-        Arrays.fill(alphabet, -1);
+        
+        for (int i = 0; i < 26; i++) {
+            alphabet[i] = -1;
+        }
+        
         for (char c : word.toCharArray()) {
             if (alphabet[c - 'a'] == -1) {
                 alphabet[c - 'a'] = word.indexOf(c);
             }
         }
 
-        StringBuilder sb = new StringBuilder();
         for (int i = 0; i < alphabet.length; i++) {
             sb.append(alphabet[i]).append(" ");
         }
