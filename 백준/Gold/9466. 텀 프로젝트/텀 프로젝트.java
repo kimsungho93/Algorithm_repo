2707f68414b91970b9228e80627aa1b091c9f35e
kimsungho93 <87847853+kimsungho93@users.noreply.github.com>
2024-04-08 23:36:38 +0900

    [Gold III] Title: 텀 프로젝트, Time: 1336 ms, Memory: 304952 KB -BaekjoonHub

---
+++ 백준/Gold/9466. 텀 프로젝트/텀 프로젝트.java
@@ -0,0 +1,54 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int[] students;
+    static boolean[] visited;
+    static boolean[] finished;
+    static int count;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+
+        while (T-- > 0) {
+            int n = Integer.parseInt(br.readLine());
+            students = new int[n + 1];
+            visited = new boolean[n + 1];
+            finished = new boolean[n + 1];
+            count = 0;
+            
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int i = 1; i <= n; i++) {
+                students[i] = Integer.parseInt(st.nextToken());
+            }
+
+            for (int i = 1; i <= n ; i++) {
+                dfs(i);
+            }
+            System.out.println(n - count);
+        }
+
+    }
+
+    private static void dfs(int now) {
+        if (visited[now]) {
+            return;
+        }
+        visited[now] = true;
+        int next = students[now];
+        if (!visited[next]) {
+            dfs(next);
+        } else {
+            if (!finished[next]) {
+                count++;
+                for (int i = next; i != now; i = students[i]) {
+                    count++;
+                }
+            }
+        }
+        finished[now] = true;
+    }
+}
